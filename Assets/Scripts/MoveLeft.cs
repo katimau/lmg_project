e26fc9e7eea417ec03f368b74b5fa3908d40157f
Salli Matinlinna <salli.matinlinna@gmail.com>
2021-10-09 21:25:59 +0300

    Ilman blokkibugia

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,7 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
     private PlayerController playerControllerScript;
-    //private float leftBound = -15;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +22,13 @@ public class MoveLeft : MonoBehaviour
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
-/*
+
         if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject); 
         }
         
-*/
+
 
 
     }
