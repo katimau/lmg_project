20aefd186bf2e6b40fb5e3aaf5366829f51cd504
Tommi Kekäläinen <tommi.kekalainen01@gmail.com>
2021-10-14 14:23:18 +0300

    Uudelleennimeä MoveLeft

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
+public class Move : MonoBehaviour
 {
     public Vector3 speed;
 
