bccef9561a333d6792d4ada2e6a39a11a698eb0e
Tuomas Kovanen <tuomas.kovanen58@gmail.com>
2021-11-17 14:11:31 +0200

    Äänet

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class CollectableItem : MonoBehaviour
 {
-    public AudioSource collectSound;
+    
+    public AudioClip aani;
+     AudioSource collectSound;
     private Scoring scoreComponent;
 
     public int points;
 
     void Start()
     {
+        collectSound = GetComponent<AudioSource>();
         scoreComponent = GameObject.Find("Score Text").GetComponent<Scoring>();
     }
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Kerätty");
         if (collectSound != null)
         {
-            collectSound.Play();
+            Debug.Log("Soitetaan");
+            collectSound.PlayOneShot(aani, 0.9F);
         }
         scoreComponent.Score += points;
-        Destroy(gameObject);
+        Destroy(gameObject, 1F);
     }
 }
