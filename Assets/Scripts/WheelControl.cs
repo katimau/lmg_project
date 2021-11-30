8b04b8297cf8b15818adc372364d45df5a412c9b
Tommi Kekäläinen <tommi.kekalainen01@gmail.com>
2021-11-30 19:04:06 +0200

    Säädä renkaiden pyörimistä

--- Assets/Scripts/WheelControl.cs
+++ Assets/Scripts/WheelControl.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class WheelControl : MonoBehaviour
 {
 
-    float RotateSpeed = 0;
-    // Start is called before the first frame update
+    public float rotateSpeed;
+    private PlayerController pc;
+
     void Start()
     {
-        
+        pc = GetComponentInParent<PlayerController>();
+        if (pc == null)
+        {
+            Debug.LogError("Parent doesn't have a PlayerController!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(-80 * Time.deltaTime, 0, 0 );
+        if (pc.gameOver == false)
+        {
+            transform.Rotate(rotateSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
